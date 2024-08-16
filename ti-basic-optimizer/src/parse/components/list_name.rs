f581d6dc8eae766520b607d52c8728ebb490474e
Ryan Pitasky <ryanpitasky+github@gmail.com>
2024-08-16 10:40:49 -0400

    parse: refactor string/matrix/list names
    It really should have been done like this from the start.

--- ti-basic-optimizer/src/parse/components/list_name.rs
+++ ti-basic-optimizer/src/parse/components/list_name.rs
@@ -4,43 +4,19 @@ use crate::parse::Parse;
 
 #[derive(Copy, Clone, Debug)]
 pub enum ListName {
-    // $5D00
-    L1,
-    // $5D01
-    L2,
-    // $5D02
-    L3,
-    // $5D03
-    L4,
-    // $5D04
-    L5,
-    // $5D05
-    L6,
-
+    Default(Token),
     Custom {
         /// Must match the TI-ASCII bytes for `[A-Zθ][A-Zθ0-9]{,4}`, and be zero
         /// filled at the end.
         name: [u8; 5],
     },
-
-    Ans,
 }
 
 impl Parse for ListName {
     fn parse(token: Token, tokens: &mut Tokens) -> Option<Self> {
         match token {
             // 5Dxx, lists
-            Token::TwoByte(0x5D, x) if x < 0x06 => match x {
-                0x00 => Some(ListName::L1),
-                0x01 => Some(ListName::L2),
-                0x02 => Some(ListName::L3),
-                0x03 => Some(ListName::L4),
-                0x04 => Some(ListName::L5),
-                0x05 => Some(ListName::L6),
-                // # Safety
-                // x < 0x06 condition above prevents this from being reachable.
-                _ => unsafe { core::hint::unreachable_unchecked() },
-            },
+            Token::TwoByte(0x5D, 0x00..=0x05) => Some(ListName::Default(token)),
 
             // EB, |L
             Token::OneByte(0xEB) => {
